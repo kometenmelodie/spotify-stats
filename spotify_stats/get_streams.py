555ccb8d1ed409f47388db9de64db2fe00652909
Jakob Klotz <88966745+kometenmelodie@users.noreply.github.com>
2022-10-08 17:07:40 +0200

    feat: concat streaming history

---
+++ spotify_stats/get_streams.py
@@ -0,0 +1,33 @@
+from os import listdir
+import pandas as pd
+
+
+def get_streams(path: str) -> pd.DataFrame:
+    """
+    Construct a data frame for all given json files.
+    The data frame contains the streaming history sorted
+    by the date a song was played.
+    """
+    files = [path + x for x in listdir(path)
+             if x.startswith("endsong_")]
+
+    files = sorted(files)
+
+    # empty list to store data frames
+    dfs = []
+    for file in files:
+        data = pd.read_json(file)
+        dfs.append(data)
+
+    df = pd.concat(dfs, ignore_index=True)
+
+    # sort by timestamp
+    df = df.sort_values(by=["ts"])
+
+    # calculate seconds played
+    df["seconds_played"] = df.ms_played / 1000
+
+    # calculate minutes played
+    df["minutes_played"] = df.seconds_played / 60
+
+    return df
