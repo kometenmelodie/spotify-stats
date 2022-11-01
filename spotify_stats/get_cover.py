6eeccfc5ff1e2278484e06cdc36d713f0cb11e37
Jakob Klotz <88966745+kometenmelodie@users.noreply.github.com>
2022-11-01 11:08:41 +0100

    feat: retrieve URL to track cover

--- spotify_stats/get_cover.py
+++ spotify_stats/get_cover.py
@@ -6,7 +6,7 @@ from PIL import Image
 def get_cover_image(
         track_uri: str, spotify_credentials: spotipy.client.Spotify,
         out_path: str = None
-) -> None:
+) -> Image:
     """
     Use spotipy to retrieve a cover as PNG of a Spotify track URI.
     Needs spotify developer credentials.
@@ -24,7 +24,25 @@ def get_cover_image(
     cover = Image.open(response.raw)
 
     if out_path is not None:
-
         cover.save(out_path, format="PNG")
 
     return cover
+
+
+def get_cover_url(
+        track_uri: str, spotify_credentials: spotipy.client.Spotify
+) -> str:
+    """
+    Get link to the cover in order to display it in html.
+    """
+
+    # get track info
+    track = spotify_credentials.track(track_uri)
+
+    # url to smaller cover
+    cover_url = track["album"]["images"][1]["url"]
+
+    # return as html
+    cover_html = "<img src='" + cover_url + "'>"
+
+    return cover_html
